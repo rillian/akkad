888eeeead3a035efbc38bad05623a68b48c7f9e5
Ralph Giles <giles@thaumas.net>
2015-03-18 16:47:21 -0700

    Add a sign list from Huehnergard Chapter 9.
    Should probably invert this since most signs have more than
    one reading.

--- glyph.js
+++ glyph.js
@@ -1,6 +1,30 @@
 // Convert Akkadian transliteration to unicode cuneiform.
 // Copyright 2015 Ralph Giles. GPLv3.
 
+
+var akkad_table = {
+  'aš': '𒀸',
+  'rum': '𒀸',
+  'ḫal': '𒄬',
+  'an': '𒀭',
+  'maḫ': '𒈤',
+  'la': '𒆷',
+  'be': '𒍗', // or 𒁁?
+  'bad': '𒍗',
+  'bat': '𒍗',
+  'baṭ': '𒍗',
+  'til': '𒍗',
+  'mu': '𒈬',
+  'dim': '𒁴',
+  'tim': '𒁴',
+  'ṭim': '𒁴',
+  'dem': '𒁴',
+  'tem': '𒁴',
+  'ṭem': '𒁴',
+  'na': '𒈾',
+  'nu': '𒉡',
+};
+
 var convert = function(text) {
   var wedges = [];
   for (word of text.split(' ')) {
