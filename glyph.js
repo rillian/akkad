400b475b5efc009a15a6151bed1b6374dc8a07fa
Ralph Giles <giles@thaumas.net>
2015-03-18 13:58:56 -0700

    Proof of context conversion.

---
+++ glyph.js
@@ -0,0 +1,18 @@
+// Convert Akkadian transliteration to unicode cuneiform.
+// Copyright 2015 Ralph Giles. GPLv3.
+
+var convert = function(text) {
+  var wedges = [];
+  for (word of text.split(' ')) {
+    var code = '';
+    for (sign of word.split('-')) {
+      if (sign == 'an') {
+        code += '𒀭';
+      } else {
+        code += sign;
+      }
+    }
+    wedges.push(code)
+  }
+  return wedges.join();
+}
