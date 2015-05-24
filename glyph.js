f8d5c50e91776c5697321131c10206eb1785e3e2
Ralph Giles <giles@thaumas.net>
2015-05-24 17:22:57 +0000

    Invert readings table in code.
    Most signs have multiple readings, so this makes entry
    of new symbols easier. We just invert the table to build
    what we need to look up input values at load time.

--- glyph.js
+++ glyph.js
@@ -5,54 +5,38 @@
 // 3rd Edition, compiled by Angelika and published with
 // the AkkadianStudy Yahoo Group. Unicode values from
 // http://www.unicode.org/charts/PDF/U12000.pdf
-var akkad_table = {
-  'aš': '𒀸',
-  'rum': '𒀸',
-  'ḫal': '𒄬',
-  'an': '𒀭',
-  'maḫ': '𒈤',
-  'la': '𒆷',
-  'be': '𒍗', // or 𒁁?
-  'bad': '𒍗',
-  'bat': '𒍗',
-  'baṭ': '𒍗',
-  'til': '𒍗',
-  'mu': '𒈬',
-  'dim': '𒁴',
-  'tim': '𒁴',
-  'ṭim': '𒁴',
-  'dem': '𒁴',
-  'tem': '𒁴',
-  'ṭem': '𒁴',
-  'na': '𒈾',
-  'nu': '𒉡',
-  'ti': '𒋾',
-  'ṭì': '𒋾',
-  'ḫu': '𒄷',
-  'nam': '𒉆',
-  'en': '𒂗',
-  'ri': '𒊑',
-  're': '𒊑',
-  'tal': '𒊑',
-  'ṭal': '𒊑',
-  'zi': '𒍣',
-  'ze': '𒍣',
-  'sí': '𒍣',
-  'sé': '𒍣',
-  'ṣí': '𒍣',
-  'ṣé': '𒍣',
-  'gi': '𒄀',
-  'ge': '𒄀',
-  'ag': '𒀝',
-  'ak': '𒀝',
-  'aq': '𒀝',
-  'ig': '𒅅',
-  'ik': '𒅅',
-  'iq': '𒅅',
-  'eg': '𒅅',
-  'ek': '𒅅',
-  'eq': '𒅅',
+var akkad_readings = {
+
+  // Lesson 9 signs.
+  '𒀸': ['aš', 'rum'],
+  '𒄬': ['ḫal'],
+  '𒀭': ['an'],
+  '𒈤': ['maḫ'],
+  '𒆷': ['la'],
+  '𒉡': ['nu'],
+  '𒁴': ['dim', 'tim', 'ṭim', 'dem', 'tem', 'ṭem'],
+  '𒍗': ['be', 'bad', 'bat', 'baṭ', 'til'], // or 𒁁?
+  '𒈾': ['na'],
+  '𒈬': ['mu'],
+
+  // Lesson 10
+  '𒋾': ['ti', 'tì'],
+  '𒄷': ['ḫu'],
+  '𒉆': ['nam'],
+  '𒂗': ['en'],
+  '𒊑': ['ri', 're', 'tal', 'ṭal'],
+  '𒍣': ['zi', 'ze', 'sí', 'sé', 'ṣí', 'ṣé'],
+  '𒄀': ['gi', 'ge'],
+  '𒀝': ['ag', 'ak', 'aq'],
+  '𒅅': ['ig', 'ik', 'iq', 'eg', 'ek', 'eq'],
 };
+
+var akkad_table = {};
+for (sign in akkad_readings) {
+  for (reading of akkad_readings[sign]) {
+    akkad_table[reading] = sign;
+  }
+}
 
 var convert = function(text) {
   var wedges = [];
